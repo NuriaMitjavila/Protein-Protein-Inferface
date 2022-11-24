3e7f6cf3cb252aba8ecc45a2c139c293901f4e21
Núria Mitjavila <109588016+NuriaMitjavila@users.noreply.github.com>
2022-11-24 09:10:15 +0100

    Add files via upload

---
+++ Step5/EXECUTION.Delta/execution.py
@@ -0,0 +1,11 @@
+import os
+import sys
+from pathlib import Path
+
+home = str(Path.home())
+myPath = os.path.join(home, "Downloads", "EXECUTION.Delta", "Scripts")
+sys.path.insert(1, myPath)
+
+import structure_checking
+import interaction_energy
+import ala_scanning
